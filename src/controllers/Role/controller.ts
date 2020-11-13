7297cc7ddee54dc6bee0cc2b5de28d29fcd6fe1b
masb0ymas <fajriajjha@gmail.com>
2020-11-13 20:42:16 +0700

    feat: crud role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -8,7 +8,7 @@ const RoleService = new Role()
 
 routes.get(
   '/role',
-  asyncHandler(async function getPosts(req: Request, res: Response) {
+  asyncHandler(async function getAll(req: Request, res: Response) {
     const query = req.getQuery()
 
     const resData = await RoleService.getAll(query)
@@ -21,14 +21,52 @@ routes.get(
 
 routes.get(
   '/role/:id',
-  asyncHandler(async function getPosts(req: Request, res: Response) {
+  asyncHandler(async function getOne(req: Request, res: Response) {
     const { id } = req.getParams()
 
     const resData = await RoleService.getOne(id)
-    console.log(resData)
-
     const { message, data } = resData.data
     const buildResponse = BuildResponse.get({ message, data })
+
+    return res.status(200).json(buildResponse)
+  })
+)
+
+routes.post(
+  '/role',
+  asyncHandler(async function createData(req: Request, res: Response) {
+    const formData = req.getBody()
+
+    const resData = await RoleService.create(formData)
+    const { message, data } = resData.data
+    const buildResponse = BuildResponse.created({ message, data })
+
+    return res.status(201).json(buildResponse)
+  })
+)
+
+routes.put(
+  '/role/:id',
+  asyncHandler(async function updateData(req: Request, res: Response) {
+    const formData = req.getBody()
+    const { id } = req.getParams()
+
+    const resData = await RoleService.update(formData, id)
+    const { message, data } = resData.data
+    const buildResponse = BuildResponse.updated({ message, data })
+
+    return res.status(200).json(buildResponse)
+  })
+)
+
+routes.delete(
+  '/role/:id',
+  asyncHandler(async function deleteData(req: Request, res: Response) {
+    const { id } = req.getParams()
+
+    const resData = await RoleService.destroy(id)
+    const { message } = resData.data
+    const buildResponse = BuildResponse.deleted({ message })
 
     return res.status(200).json(buildResponse)
   })
