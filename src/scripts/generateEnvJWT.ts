5273ca691ac39f47b137b72b5c361589ea92395b
masb0ymas <fajriajjha@gmail.com>
2020-11-28 10:58:37 +0700

    fix: generate access token & refresh token

--- src/scripts/generateEnvJWT.ts
+++ src/scripts/generateEnvJWT.ts
@@ -12,15 +12,18 @@ const getUniqueCodev2 = (length = 32) => {
   return result
 }
 
-function main() {
+function accessTokenJWT() {
   const pathRes = path.resolve('.env')
   const contentEnv = fs.readFileSync(pathRes, { encoding: 'utf-8' })
   const jwtSecret = getUniqueCodev2()
-  const strJWT = `JWT_SECRET=${jwtSecret}`
+  const strJWT = `JWT_SECRET_ACCESS_TOKEN=${jwtSecret}`
 
-  if (contentEnv.includes('JWT_SECRET=')) {
+  if (contentEnv.includes('JWT_SECRET_ACCESS_TOKEN=')) {
     // Replace JWT SECRET jika sudah ada
-    const replaceContent = contentEnv.replace(/JWT_SECRET=(.*)?/, strJWT)
+    const replaceContent = contentEnv.replace(
+      /JWT_SECRET_ACCESS_TOKEN=(.*)?/,
+      strJWT
+    )
     fs.writeFileSync(`${pathRes}`, replaceContent)
     console.log('Refresh JWT SECRET Success')
   } else {
@@ -31,4 +34,27 @@ function main() {
   }
 }
 
-main()
+function refreshTokenJWT() {
+  const pathRes = path.resolve('.env')
+  const contentEnv = fs.readFileSync(pathRes, { encoding: 'utf-8' })
+  const jwtSecret = getUniqueCodev2()
+  const strJWT = `JWT_SECRET_REFRESH_TOKEN=${jwtSecret}`
+
+  if (contentEnv.includes('JWT_SECRET_REFRESH_TOKEN=')) {
+    // Replace JWT SECRET REFRESH TOKEN jika sudah ada
+    const replaceContent = contentEnv.replace(
+      /JWT_SECRET_REFRESH_TOKEN=(.*)?/,
+      strJWT
+    )
+    fs.writeFileSync(`${pathRes}`, replaceContent)
+    console.log('Refresh JWT SECRET REFRESH TOKEN Success')
+  } else {
+    // Generate JWT SECRET REFRESH TOKEN kalo belum ada di environment
+    const extraContent = `${strJWT}\n\n${contentEnv}`
+    fs.writeFileSync(`${pathRes}`, extraContent)
+    console.log('Generate JWT SECRET REFRESH TOKEN Success')
+  }
+}
+
+accessTokenJWT()
+refreshTokenJWT()
