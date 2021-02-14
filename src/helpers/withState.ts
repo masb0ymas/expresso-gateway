badfa010f0a3547b79c650509b26dbd88c7b5769
masb0ymas <fajriajjha@gmail.com>
2021-02-14 21:26:09 +0700

    fix: helper with state

--- src/helpers/withState.ts
+++ src/helpers/withState.ts
@@ -19,7 +19,8 @@ class withState {
     this.req.setBody = this.setBody.bind(this)
     this.req.getSingleArrayFile = this.getSingleArrayFile.bind(this)
     this.req.pickSingleFieldMulter = this.pickSingleFieldMulter.bind(this)
-    this.req._transaction = {}
+    this.req.getMultiArrayFile = this.getMultiArrayFile.bind(this)
+    this.req.pickMultiFieldMulter = this.pickMultiFieldMulter.bind(this)
   }
 
   setState(val: object) {
@@ -74,8 +75,19 @@ class withState {
     }
   }
 
+  getMultiArrayFile(name: string) {
+    const data = get(this.req.files, name, []) as Express.Multer.File
+    if (data) {
+      return data
+    }
+  }
+
   pickSingleFieldMulter(fields: string[]) {
     return Multers.pickSingleFieldMulter(this.req, fields)
+  }
+
+  pickMultiFieldMulter(fields: string[]) {
+    return Multers.pickMultiFieldMulter(this.req, fields)
   }
 }
 
