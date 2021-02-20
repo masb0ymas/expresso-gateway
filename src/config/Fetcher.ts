3de58bb2d533a52dad7e0472424f4ad3e3c13198
masb0ymas <fajriajjha@gmail.com>
2021-02-20 20:44:38 +0700

    fix: config fetcher

--- src/config/Fetcher.ts
+++ src/config/Fetcher.ts
@@ -8,7 +8,7 @@ const AXIOS_TIMEOUT = process.env.AXIOS_TIMEOUT || 5000
  *
  * @param baseURL
  */
-function createAuthAxios(baseURL: string): AxiosInstance {
+function createAxios(baseURL: string): AxiosInstance {
   const instanceAxios = axios.create({
     baseURL,
     timeout: Number(AXIOS_TIMEOUT),
@@ -65,91 +65,26 @@ function createAuthAxios(baseURL: string): AxiosInstance {
   return instanceAxios
 }
 
-/**
- *
- * @param baseURL
- */
-function createDefaultAxios(baseURL: string): AxiosInstance {
-  const instanceAxios = axios.create({
-    baseURL,
-    timeout: Number(AXIOS_TIMEOUT),
-  })
-
-  instanceAxios.interceptors.response.use(
-    function onSuccess(response) {
-      return response
-    },
-    function onError(error: AxiosError) {
-      const statusCode = get(error, 'response.status', null)
-      const message = get(error, 'response.data.message', null)
-
-      if (statusCode === 401) {
-        console.log('Unauhtorized')
-        throw new ResponseError.Unauthorized(message)
-      }
-
-      if (statusCode === 400) {
-        console.log('Bad Request')
-        throw new ResponseError.BadRequest(message)
-      }
-
-      if (statusCode === 404) {
-        console.log('Not Found')
-        throw new ResponseError.NotFound(message)
-      }
-
-      const handleError = error?.response?.headers?.handleError
-      if (!handleError || !handleError(error)) {
-        if (error.code === 'ECONNREFUSED') {
-          throw new ResponseError.InternalServer('service unavailable')
-        }
-
-        console.log(error.message)
-        throw new ResponseError.BadRequest(error.message)
-      }
-      return Promise.reject(error)
-    }
-  )
-
-  return instanceAxios
-}
-
 class FetchApi {
   private axiosDefault: AxiosInstance
-
-  private axiosToken: AxiosInstance
 
   private baseUri: string
 
   constructor(baseUri: string) {
     // @ts-ignore
     this.axiosDefault = null
-    // @ts-ignore
-    this.axiosToken = null
     this.baseUri = baseUri
-  }
-
-  /**
-   * axios instance default
-   */
-  get default(): AxiosInstance {
-    if (!this.axiosDefault) {
-      this.axiosDefault = createDefaultAxios(this.baseUri)
-      return this.axiosDefault
-    }
-
-    return this.axiosDefault
   }
 
   /**
    * axios instance with auth token
    */
-  get withAuth(): AxiosInstance {
-    if (!this.axiosToken) {
-      this.axiosToken = createAuthAxios(this.baseUri)
-      return this.axiosToken
+  get default(): AxiosInstance {
+    if (!this.axiosDefault) {
+      this.axiosDefault = createAxios(this.baseUri)
+      return this.axiosDefault
     }
-    return this.axiosToken
+    return this.axiosDefault
   }
 }
 
