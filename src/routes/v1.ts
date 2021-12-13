eb40576b1b2a37f7968cae42361daa7629ef5161
N. Fajri <fajriajjha@gmail.com>
2021-12-13 11:39:05 +0700

    fix: route v1

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -5,4 +5,8 @@ const route = Express.Router()
 export default route
 
 require('@controllers/Auth/controller')
-require('@controllers/Role/controller')
+
+require('@controllers/Account/Notification/controller')
+require('@controllers/Account/Role/controller')
+require('@controllers/Account/Session/controller')
+require('@controllers/Account/User/controller')
